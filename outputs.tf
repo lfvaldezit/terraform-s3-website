029b1fca60d2e53ac15ab62ef9f41519b81588b2
Luis F. Valdez <lfvaldez.it@gmail.com>
2025-09-15 19:30:44 -0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,11 @@
 #   value = module.s3.s3_website_endpoint
 # }
 
-output "cfn_domain_name" {
-  value = module.cfn.cfn_domain_name
+# output "cfn_domain_name" {
+#   value = module.cfn.cfn_domain_name
+# }
+
+output "domain_validation_option" {
+  description = "Set of domain validation objects which can be used to complete certificate validation"
+  value = module.acm.domain_validation_option
 }
