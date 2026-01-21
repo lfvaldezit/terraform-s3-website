7cace7a0eea23406c103e5b6d9f0196b22ced967
Luis F. Valdez <lfvaldez.it@gmail.com>
2026-01-21 11:54:52 -0300

    Update IAM module files

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,11 +1,14 @@
 variable "aws_s3_bucket_id" {
+  description = ""
   type = string
 }
 
 variable "aws_s3_bucket_arn" {
+  description = ""
   type = string
 }
 
 variable "cloudfront_dist_arn" {
+  description = ""
   type = string
 }
