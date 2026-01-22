91fafb9ff7d69f528bf84e2e596eba7ac132578a
Luis F. Valdez <lfvaldez.it@gmail.com>
2026-01-22 12:10:15 -0300

    Update Cloudfront files

--- modules/cloudflare/variables.tf
+++ modules/cloudflare/variables.tf
@@ -14,7 +14,7 @@ variable "record_content" {
 }
 
 variable "record_name" {
-  description = "The subdomain name of the records"
+  description= "Map of sites to host. Key = identifier, subdomain = part before domain (e.g. 'site-a'), path = optional prefix in S3 bucket (e.g. 'site-a')."
   type = map(object({
     subdomain = string
     path = optional(string)
