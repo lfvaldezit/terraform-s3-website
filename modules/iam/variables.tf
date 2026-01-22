bdd5174cbb921d99ff10bdbd16f78c48d128e9f6
Luis F. Valdez <lfvaldez.it@gmail.com>
2026-01-22 12:20:40 -0300

    Update IAM module files

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -1,14 +1,14 @@
 variable "aws_s3_bucket_id" {
-  description = ""
+  description = "The ID (name) of the S3 bucket serving as the CloudFront origin"
   type = string
 }
 
 variable "aws_s3_bucket_arn" {
-  description = ""
+  description = "The full ARN of the S3 bucket"
   type = string
 }
 
 variable "cloudfront_dist_arn" {
-  description = ""
+  description = "The ARN of the existing CloudFront distribution"
   type = string
 }
