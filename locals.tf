ac257160163f0f489f6ce5027bad1817eab50ff5
Luis F. Valdez <lfvaldez.it@gmail.com>
2026-01-23 10:19:38 -0300

    Update locals.tf file

--- locals.tf
+++ locals.tf
@@ -2,25 +2,12 @@ locals {
   profile    = "default"
   aws_region = "us-east-1"
 
-  domain_name = "lfvaldez-it.link"
+  domain_name = "example.com"
 
   common_tags = {
     Owner : "admin"
     Environment : "Test"
     ManagedBy : "Terraform"
-    Project : "terraform-website"
-  }
-
-  sites = {
-    site-a = {
-      subdomain = "site-a"
-      path      = "site-a"
-    }
-
-
-    site-b = {
-      subdomain = "site-b"
-      path      = "site-b"
-    }
+    Project : "terraform-multi-sites"
   }
 }
